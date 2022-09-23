1166f99b6fe3303107cadf369df98a95fc5fcb22
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2022-09-23 10:47:54 +0000

    sample code

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-# make a function to get all repo links of a user in github
-import requests
-from bs4 import BeautifulSoup
+# sample python script
 
+import sys
 
-def get_all_repo_links(username):
-    """Get all repo links of a user in github"""
-    url = f"https://github.com/{username}?tab=repositories"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    repo_links = []
-    for link in soup.find_all("a"):
-        href = link.get("href")
-        if href.startswith(f"/{username}/"):
-            repo_links.append(f"https://github.com{href}")
-    return repo_links
-
-
-repoLinks = get_all_repo_links("OCEANOFANYTHINGOFFICIAL")
-for link in repoLinks:
-    print(link)
+def main():
+    print("Hello World!")
+    print(sys.version)
+    print("The sys path is", sys.path)
+    return 0
